844b55d403c785a1681b1958354589c877661de8
SttDmitry <sheicko.dmitry@yandex.ru>
2021-08-02 17:48:38 +0300

    hw5 without security

--- src/main/java/my/market/MarketApp.java
+++ src/main/java/my/market/MarketApp.java
@@ -2,9 +2,11 @@ package my.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableFeignClients
 public class MarketApp {
     public static void main(String[] args) {
