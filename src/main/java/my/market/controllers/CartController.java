32f1d10884c55cb13753957bf3d1ab595be141b7
SttDmitry <sheicko.dmitry@yandex.ru>
2021-08-08 15:35:15 +0300

    hw6

--- src/main/java/my/market/controllers/CartController.java
+++ src/main/java/my/market/controllers/CartController.java
@@ -4,7 +4,7 @@ import my.market.utils.ShoppingCart;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("geek-spring-cloud-eureka-client")
+//@FeignClient("geek-spring-cloud-eureka-client")
 public interface CartController {
     @RequestMapping("/save/")
     String save(ShoppingCart cart);
